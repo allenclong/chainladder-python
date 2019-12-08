5f7b9640c146496bdf078d27477785f849bb2fc3
allenclong <allenclong@gmail.com>
2019-12-07 23:10:23 -0500

    Update munich.py

--- chainladder/development/munich.py
+++ chainladder/development/munich.py
@@ -103,7 +103,8 @@ class MunichAdjustment(BaseEstimator, TransformerMixin, EstimatorIO):
         p_to_i_ldf = np.unique(p_to_i_ldf, axis=-2)  # May cause issues later
         p_to_i_sigma = np.unique(p_to_i_sigma, axis=-2)  # May cause issues
         print(p_to_i_sigma.shape)
-        if p_to_i_sigma.shape[2] == p_to_i_sigma.shape[3] or (4*p_to_i_sigma.shape[2]) == p_to_i_sigma.shape[3]+1:
+        print(paid.shape)
+        if paid.shape[2] == paid.shape[3] or (4*paid.shape[2]) == paid.shape[3]+1:
             residualP = (p_to_i_ata[0]-p_to_i_ldf[0]) / \
                 p_to_i_sigma[0]*np.sqrt(paid[..., :-1, :-1]) 
             residualI = (p_to_i_ata[1]-p_to_i_ldf[1]) / \
