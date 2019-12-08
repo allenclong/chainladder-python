864758659318cdbd0a297ed928486aa1ed1319ff
allenclong <allenclong@gmail.com>
2019-12-07 20:56:25 -0500

    Fix shape on munich

--- chainladder/development/munich.py
+++ chainladder/development/munich.py
@@ -103,7 +103,7 @@ class MunichAdjustment(BaseEstimator, TransformerMixin, EstimatorIO):
         p_to_i_ldf = np.unique(p_to_i_ldf, axis=-2)  # May cause issues later
         p_to_i_sigma = np.unique(p_to_i_sigma, axis=-2)  # May cause issues
         residualP = (p_to_i_ata[0]-p_to_i_ldf[0]) / \
-            p_to_i_sigma[0]*np.sqrt(paid[..., :-1, :-1])
+            p_to_i_sigma[0]*np.sqrt(paid[..., :-1]) # remove  :-1,
         residualI = (p_to_i_ata[1]-p_to_i_ldf[1]) / \
             p_to_i_sigma[1]*np.sqrt(incurred[..., :-1, :-1])
         nans = (X-X[X.valuation == X.valuation_date]).values[0, 0]*0+1
